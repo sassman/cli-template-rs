eec216922205995d5613ac35b6544d80df21f5d9
Sven Assmann <sven.assmann.it@gmail.com>
2020-09-22 09:15:06 +0200

    refactor(template): cleanup a little unused code and add more crate macros

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use clap::{App, Arg, SubCommand, crate_version, AppSettings};
+use clap::{crate_authors, crate_description, crate_version, App, AppSettings, Arg, SubCommand};
 
-use {{crate_name}}::*;
 use std::io;
-use std::io::stdin;
 
 fn main() -> io::Result<()> {
     let matches = App::new("{{project-name}}")
         .version(crate_version!())
-        .author("{{authors}}")
-        .about("TODO: a little description would be nice")
+        .author(crate_authors!())
+        .about(crate_description!())
         .setting(AppSettings::ArgRequiredElseHelp)
-    ).subcommand(SubCommand::with_name("TODO a basic subcommand")
+    .subcommand(SubCommand::with_name("TODO a basic subcommand")
         .about("TODO briefly what it does")
         .arg(
             Arg::with_name("argname1")
